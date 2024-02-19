aac59da1f0f98dcbad475c0a7a0cd869f5b162b6
Channing <channing.phong@gmail.com>
2024-02-18 21:31:56 -0500

    Update

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from pytube import YouTube
 import os
 
-def download(link, path):
-    yt = YouTube(link)
-    yt = yt.streams.get_highest_resolution()
-    if not os.path.exists(path):
-        os.makedirs(path)
-    try:
-        yt.download(path)
-    except:
-        print('An error has occurred.')
-    print('Download is completed successfully.')
+while True:
+    def download(link, path):
+        yt = YouTube(link)
+        yt = yt.streams.get_highest_resolution()
+        if not os.path.exists(path):
+            os.makedirs(path)
+        try:
+            yt.download(path)
+        except:
+            print('An error has occurred.')
+        print('Download is completed successfully.')
 
-link = input('Enter the YouTube video URL: ')
-path = input('Enter the path to store file: ')
-download(link, path)
+    link = input('Enter the YouTube video URL: ')
+    path = input('Enter the path to store file: ')
+    download(link, path)
+
+    downloadAgain = input('Do you want to download another video? (yes/no) ').lower()
+    if downloadAgain != 'yes':
+        break
